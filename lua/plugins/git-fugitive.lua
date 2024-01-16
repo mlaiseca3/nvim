3ac271916076a8e850e4afe279756ec8b9f5c03d
Mario Laiseca-Ruiz <mariolaisecaruiz@gmail.com>
2024-01-15 21:02:25 -0600

    updating keymaps for telescope and git-fugitive plugins

--- lua/plugins/git-fugitive.lua
+++ lua/plugins/git-fugitive.lua
@@ -1,10 +1,8 @@
 return {
 	"tpope/vim-fugitive",
-	keys = {
-		{"<leader>g", "<cmd>Git<cr>"},
-	},
 	config = function ()
 		-- no setup function call required
-	end
+	end,
 
+	vim.keymap.set('n', '<leader>g', vim.cmd.Git)
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,6 +20,17 @@ return {
 			vim.keymap.set('n', '<leader><leader>', builtin.buffers, {desc = '[ ] Search open buffers'})
 			vim.keymap.set('n', '<leader>sh', builtin.help_tags, {desc = '[S]earch [H]elp tags'})
 			vim.keymap.set('n', '<leader>sw', builtin.grep_string, {desc = '[S]earch current [W]ord'})
+			vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+			vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+			vim.keymap.set('n', '<leader>/', function()
+				-- You can pass additional configuration to telescope to change theme, layout, etc.
+				require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+					winblend = 10,
+					previewer = false,
+				})
+			end, { desc = '[/] Fuzzily search in current buffer]' })
+			vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, {desc = '[D]ocument [S]ymbols'})
+			vim.keymap.set('n', '<F3>', builtin.diagnostics, {desc = 'diagnostics for current buffer'})
 
 			require("telescope").load_extension("ui-select")
 		end,
