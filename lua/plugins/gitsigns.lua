5b87f848dc5ce7ce5f02e30cf1944b5e4130e6e6
Mario Laiseca-Ruiz <mariolaisecaruiz@gmail.com>
2024-09-17 15:46:56 -0500

    git hunk traversal

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -13,6 +13,31 @@ return {
 				topdelete = { text = '‾' },
 				changedelete = { text = '~' },
 			},
+			on_attach = function(bufnr)
+				local gitsigns = require('gitsigns')
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+				-- Navigation
+				map('n', '<leader>]', function()
+					if vim.wo.diff then
+						vim.cmd.normal({'<leader>]', bang = true})
+					else
+						gitsigns.nav_hunk('next')
+					end
+				end)
+
+				map('n', '<leader>[', function()
+					if vim.wo.diff then
+						vim.cmd.normal({'<leader>[', bang = true})
+					else
+						gitsigns.nav_hunk('prev')
+					end
+				end)
+			end,
 		}
 	end,
 }
