6ea5502e2b3bbfe3d8dc72fdd800e578382ec179
Mario Laiseca-Ruiz <mariolaisecaruiz@gmail.com>
2024-10-09 11:41:40 -0500

    added tab search and picker functionality

---
+++ lua/plugins/tabs.lua
@@ -0,0 +1,11 @@
+return {
+	'LukasPietzschmann/telescope-tabs',
+	config = function()
+		require('telescope').load_extension 'telescope-tabs'
+		require('telescope-tabs').setup {
+			-- Your custom config :^)
+		}
+		vim.keymap.set("n", "<leader>t", function() require('telescope-tabs').list_tabs() end)
+	end,
+	dependencies = { 'nvim-telescope/telescope.nvim' },
+}
