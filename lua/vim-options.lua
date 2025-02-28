5af2d95b232ce8e05881b0b39d999a494d1d456b
Laiseca-Ruiz, Mario <mario.laisecaruiz@uth.tmc.edu>
2025-02-28 10:48:46 -0600

    added some new plugins: grug-far, which-key

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,7 +3,7 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = ','
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
 vim.keymap.set('n', '<C-t>', vim.cmd.tabnew)
 
